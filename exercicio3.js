86a455b488b4e1298e6a33a762781931c8fae430
LucasKeller7 <67236221+LucasKeller7@users.noreply.github.com>
2022-06-23 09:27:16 -0300

    Add files via upload

---
+++ exercicio3.js
@@ -0,0 +1,10 @@
+salario1 = 3500
+salario2 = 4200
+salario3 = 2200
+salario4 = 1500
+salario5 = 2000
+ 
+let salariototal = salario1 + salario2 + salario3 + salario4 + salario5
+let media = salariototal / 5
+console.log(media)
+
